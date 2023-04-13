efbfd82ae2a56d125136635134b43faffc0a0a2e
chunningham <c.a.cunningham6@gmail.com>
2023-04-13 22:11:28 +1000

    move varsig trait to /traits, impl wrapper

--- varsig/src/either.rs
+++ varsig/src/either.rs
@@ -1,5 +1,5 @@
-use crate::{Error, SignatureHeader};
-use std::io::{Error as IoError, Read, Write};
+use crate::{DeserError, SerError, VarSigTrait};
+use std::io::{Read, Write};
 
 pub enum EitherSignature<A, B> {
     A(A),
@@ -14,28 +14,62 @@ pub enum EitherError<A, B> {
     B(B),
 }
 
-impl<A, B> SignatureHeader for EitherSignature<A, B>
+impl<A, B> VarSigTrait for EitherSignature<A, B>
 where
-    A: SignatureHeader,
-    B: SignatureHeader,
+    A: VarSigTrait,
+    B: VarSigTrait,
 {
     type SerError = EitherError<A::SerError, B::SerError>;
     type DeserError = EitherError<A::DeserError, B::DeserError>;
-    fn from_reader<R>(reader: &mut R) -> Result<Self, Self::DeserError>
+    fn valid_header(bytes: &[u8]) -> bool {
+        A::valid_header(bytes) || B::valid_header(bytes)
+    }
+
+    fn from_reader<R>(reader: &mut R) -> Result<Self, DeserError<Self::DeserError>>
     where
         R: Read,
         Self: Sized,
     {
-        todo!()
+        // check the header to discern the sig type
+        let mut buf = [0u8; 4];
+        reader.read_exact(&mut buf)?;
+
+        if A::valid_header(&buf) {
+            let chain = &mut buf.chain(reader);
+            A::from_reader(chain)
+                .map_err(|e| match e {
+                    DeserError::Io(e) => DeserError::Io(e),
+                    DeserError::InvalidHeader => DeserError::InvalidHeader,
+                    DeserError::Format(e) => DeserError::Format(EitherError::A(e)),
+                })
+                .map(EitherSignature::A)
+        } else if B::valid_header(&buf) {
+            let chain = &mut buf.chain(reader);
+            B::from_reader(chain)
+                .map_err(|e| match e {
+                    DeserError::Io(e) => DeserError::Io(e),
+                    DeserError::InvalidHeader => DeserError::InvalidHeader,
+                    DeserError::Format(e) => DeserError::Format(EitherError::B(e)),
+                })
+                .map(EitherSignature::B)
+        } else {
+            Err(DeserError::InvalidHeader)
+        }
     }
 
-    fn to_writer<W>(&self, writer: &mut W) -> Result<(), Self::SerError>
+    fn to_writer<W>(&self, writer: &mut W) -> Result<(), SerError<Self::SerError>>
     where
         W: ?Sized + Write,
     {
         match self {
-            Self::A(a) => a.to_writer(writer).map_err(EitherError::A),
-            Self::B(b) => b.to_writer(writer).map_err(EitherError::B),
+            Self::A(a) => a.to_writer(writer).map_err(|e| match e {
+                SerError::Io(e) => SerError::Io(e),
+                SerError::Format(e) => SerError::Format(EitherError::A(e)),
+            }),
+            Self::B(b) => b.to_writer(writer).map_err(|e| match e {
+                SerError::Io(e) => SerError::Io(e),
+                SerError::Format(e) => SerError::Format(EitherError::B(e)),
+            }),
         }
     }
 }
--- varsig/src/lib.rs
+++ varsig/src/lib.rs
@@ -1,154 +1,120 @@
 use std::io::{Read, Write};
-use unsigned_varint::{
-    encode::{u64 as write_u64, u64_buffer},
-    io::read_u64,
-};
+
 pub mod eip191;
 pub mod either;
+pub mod traits;
 
 pub use either::EitherSignature;
+pub use traits::{DeserError, SerError, VarSigTrait};
 
 const VARSIG_VARINT_PREFIX: u8 = 0x34;
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct VarSig<H> {
-    codec: u64,
-    hash: u64,
-    signature: H,
-}
+pub struct VarSig<S: VarSigTrait>(S);
 
-pub trait SignatureHeader {
-    type SerError: std::error::Error + std::fmt::Debug;
-    type DeserError: std::error::Error + std::fmt::Debug;
-    fn from_reader<R>(reader: &mut R) -> Result<Self, Self::DeserError>
+impl<S: VarSigTrait> VarSig<S> {
+    pub fn new(s: S) -> Self {
+        Self(s)
+    }
+
+    pub fn sig(&self) -> &S {
+        &self.0
+    }
+
+    pub fn from_reader<R>(reader: &mut R) -> Result<Self, Error<S::DeserError>>
     where
         Self: Sized,
-        R: Read;
-    fn to_writer<W>(&self, writer: &mut W) -> Result<(), Self::SerError>
-    where
-        W: ?Sized + Write;
-}
-
-#[derive(thiserror::Error, Debug)]
-pub enum Error<S: SignatureHeader> {
-    #[error(transparent)]
-    Varint(#[from] unsigned_varint::io::ReadError),
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
-    #[error("Invalid varsig prefix, expected 0x34, recieved {0:x}")]
-    InvalidPrefix(u8),
-    #[error(transparent)]
-    SignatureDeser(S::DeserError),
-    #[error(transparent)]
-    SignatureSer(S::SerError),
-}
-
-impl<H> VarSig<H> {
-    pub fn new(codec: u64, hash: u64, signature: H) -> Self {
-        Self {
-            codec,
-            hash,
-            signature,
+        R: Read,
+    {
+        let mut buf = [0u8; 1];
+        reader.read_exact(&mut buf)?;
+        if buf[0] != VARSIG_VARINT_PREFIX {
+            return Err(Error::InvalidPrefix(buf[0]));
         }
+        Ok(Self::new(S::from_reader(reader)?))
     }
 
-    pub fn codec(&self) -> u64 {
-        self.codec
+    pub fn to_writer<W>(&self, writer: &mut W) -> Result<(), SerError<S::SerError>>
+    where
+        W: ?Sized + Write,
+    {
+        writer.write_all(&[VARSIG_VARINT_PREFIX])?;
+        self.0.to_writer(writer)
     }
 
-    pub fn hash(&self) -> u64 {
-        self.hash
-    }
-
-    pub fn signature(&self) -> &H {
-        &self.signature
-    }
-}
-
-impl<H> VarSig<H>
-where
-    H: SignatureHeader,
-{
-    pub fn to_vec(&self) -> Result<Vec<u8>, Error<H>> {
+    pub fn to_vec(&self) -> Result<Vec<u8>, SerError<S::SerError>> {
         let mut buf = Vec::new();
         self.to_writer(&mut buf)?;
         Ok(buf)
     }
 
-    pub fn from_bytes(bytes: &[u8]) -> Result<Self, Error<H>> {
-        Self::from_reader(&mut bytes.as_ref())
-    }
-
-    pub fn from_reader<R>(reader: &mut R) -> Result<Self, Error<H>>
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, Error<S::DeserError>>
     where
-        R: Read,
+        Self: Sized,
     {
-        let mut tag = [0u8; 1];
-        reader.read_exact(&mut tag)?;
-
-        if tag[0] != VARSIG_VARINT_PREFIX {
-            return Err(Error::InvalidPrefix(tag[0]));
-        };
-
-        let codec = read_u64(reader.by_ref())?;
-        let hash = read_u64(reader.by_ref())?;
-        let signature = H::from_reader(reader.by_ref()).map_err(Error::SignatureDeser)?;
-
-        Ok(Self::new(codec, hash, signature))
+        let mut reader = bytes;
+        Self::from_reader(&mut reader)
     }
+}
 
-    pub fn to_writer<W>(&self, writer: &mut W) -> Result<(), Error<H>>
-    where
-        W: ?Sized + Write,
-    {
-        writer.write(&[VARSIG_VARINT_PREFIX; 1])?;
-        let mut buf = u64_buffer();
-        writer.write(write_u64(self.codec, &mut buf))?;
-        writer.write(write_u64(self.hash, &mut buf))?;
-        self.signature
-            .to_writer(writer)
-            .map_err(Error::SignatureSer)?;
-        Ok(())
+#[derive(thiserror::Error, Debug)]
+pub enum Error<D> {
+    #[error(transparent)]
+    Format(D),
+    #[error("Invalid Prefix, expected 0x34, got 0x{0:x}")]
+    InvalidPrefix(u8),
+    #[error("Invalid header")]
+    InvalidHeader,
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+}
+
+impl<D> From<DeserError<D>> for Error<D> {
+    fn from(e: DeserError<D>) -> Self {
+        match e {
+            DeserError::Format(e) => Error::Format(e),
+            DeserError::InvalidHeader => Error::InvalidHeader,
+            DeserError::Io(e) => Error::IoError(e),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
-    const EXAMPLE: [u8; 15] = [
-        0x68, 0xd2, 0x04, 0xda, 0x03, 0x24, 0xde, 0xb7, 0xde, 0x9a, 0xf1, 0xd9, 0xa2, 0xa3, 0x02,
-    ];
+    // const EXAMPLE: [u8; 15] = [
+    //     0x68, 0xd2, 0x04, 0xda, 0x03, 0x24, 0xde, 0xb7, 0xde, 0x9a, 0xf1, 0xd9, 0xa2, 0xa3, 0x02,
+    // ];
 
-    #[test]
-    fn basic_roundtrip() {
-        let varsig = VarSig::from_reader(&mut EXAMPLE.as_ref()).unwrap();
-        assert_eq!(varsig.codec(), 0x04d2);
-        assert_eq!(varsig.hash(), 0x03da);
-        assert_eq!(varsig.key_type(), 0x24);
-        assert_eq!(varsig.signature(), &EXAMPLE[6..]);
+    // #[test]
+    // fn basic_roundtrip() {
+    //     let varsig = VarSigTrait::from_reader(&mut EXAMPLE.as_ref()).unwrap();
+    //     assert_eq!(varsig.codec(), 0x04d2);
+    //     assert_eq!(varsig.hash(), 0x03da);
+    //     assert_eq!(varsig.key_type(), 0x24);
+    //     assert_eq!(varsig.signature(), &EXAMPLE[6..]);
 
-        assert_eq!(&varsig.to_vec().unwrap(), &EXAMPLE);
-    }
+    //     assert_eq!(&varsig.to_vec().unwrap(), &EXAMPLE);
+    // }
 
-    #[test]
-    fn reverse_roundtrip() {
-        let varsig = VarSig::new(0x0129, 0x12, 0xed, EXAMPLE[6..].to_vec());
-        let encoded = varsig.to_vec().unwrap();
-        let decoded = VarSig::from_bytes(&encoded.as_ref()).unwrap();
-        assert_eq!(varsig, decoded);
-    }
+    // #[test]
+    // fn reverse_roundtrip() {
+    //     let varsig = VarSigTrait::new(0x0129, 0x12, 0xed, EXAMPLE[6..].to_vec());
+    //     let encoded = varsig.to_vec().unwrap();
+    //     let decoded = VarSigTrait::from_bytes(&encoded.as_ref()).unwrap();
+    //     assert_eq!(varsig, decoded);
+    // }
 
-    #[test]
-    fn basic_ser() {
-        let varsig = VarSig::new(0x0129, 0x12, 0xed, EXAMPLE[6..].to_vec());
-        assert_eq!(&varsig.to_vec().unwrap(), &EXAMPLE);
-    }
+    // #[test]
+    // fn basic_ser() {
+    //     let varsig = VarSigTrait::new(0x0129, 0x12, 0xed, EXAMPLE[6..].to_vec());
+    //     assert_eq!(&varsig.to_vec().unwrap(), &EXAMPLE);
+    // }
 
-    #[test]
-    fn rsad() {
-        let b = [0xd2, 0x04];
-        println!("{:#x?}", read_u64(&mut b.as_ref()));
-        None::<u8>.unwrap();
-    }
+    // #[test]
+    // fn rsad() {
+    //     let b = [0xd2, 0x04];
+    //     println!("{:#x?}", read_u64(&mut b.as_ref()));
+    //     None::<u8>.unwrap();
+    // }
 }
