0084986f8759b3d5f143210466f8473e856e9c13
chunningham <c.a.cunningham6@gmail.com>
2023-09-10 12:53:12 +0200

    Common fixes and utilities

--- src/v2/common.rs
+++ src/v2/common.rs
@@ -6,8 +6,8 @@ use super::{
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use ssi_dids::did_resolve::DIDResolver;
-use varsig::{either::EitherSignature, VarSig};
+use ssi_ucan::{common::Common, jose::Signature, jwt::UcanEncode, Payload, Ucan};
+use varsig::{common::JoseSig, either::EitherSignature, VarSig};
 
 type CommonSignature = EitherSignature<RecapSignature, UcanSignature>;
 
@@ -28,30 +28,54 @@ impl<T> CommonVerifier<T> {
     }
 }
 
+impl<F, NB> CommonCacao<F, NB>
+where
+    F: Clone + Serialize,
+    NB: Clone + Serialize,
+{
+    pub fn serialize_jwt(&self) -> Result<Option<String>, Error> {
+        Ok(match self.signature.sig() {
+            CommonSignature::A(_) => None,
+            CommonSignature::B(_) => self
+                .clone()
+                .get_ucan()
+                .map(|ucan| ucan.encode())
+                .transpose()?,
+        })
+    }
+}
+
 #[derive(thiserror::Error, Debug)]
-pub enum Error {
+pub enum Error<U: std::error::Error = UcanError> {
     #[error(transparent)]
-    Ucan(#[from] UcanError),
+    Ucan(U),
     #[error(transparent)]
     Recap(#[from] RecapError),
     #[error("Signature and Facts Mismatch")]
     Mismatch,
 }
 
-#[async_trait]
-impl<NB, R, F> CacaoVerifier<CommonSignature, CommonFacts<F>, NB> for CommonVerifier<R>
+impl From<ssi_ucan::Error> for Error {
+    fn from(e: ssi_ucan::Error) -> Self {
+        Self::Ucan(e.into())
+    }
+}
+
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+impl<'r, NB, R, F> CacaoVerifier<CommonSignature, CommonFacts<F>, NB> for CommonVerifier<R>
 where
-    R: Send + Sync + DIDResolver,
+    R: 'r + Send + Sync + CacaoVerifier<UcanSignature, UcanFacts<F>, NB>,
     F: Send + Sync + for<'a> Deserialize<'a> + Serialize + Clone,
     NB: Send + Sync + for<'a> Deserialize<'a> + Serialize + Clone,
 {
-    type Error = Error;
+    type Error = Error<R::Error>;
 
     async fn verify(&self, cacao: &CommonCacao<F, NB>) -> Result<(), Self::Error> {
         Ok(match RecapCacao::try_from(cacao.clone()) {
             Ok(recap) => self.verify(&recap).await?,
             Err(c) => match UcanCacao::try_from(c) {
-                Ok(ucan) => self.verify(&ucan).await?,
+                Ok(ucan) => self.verify(&ucan).await.map_err(Error::Ucan)?,
                 Err(_) => {
                     return Err(Error::Mismatch);
                 }
@@ -60,10 +84,11 @@ where
     }
 }
 
-#[async_trait]
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
 impl<NB, R> CacaoVerifier<RecapSignature, RecapFacts, NB> for CommonVerifier<R>
 where
-    R: Send + Sync + DIDResolver,
+    R: Send + Sync,
     NB: Send + Sync + for<'a> Deserialize<'a> + Serialize + Clone,
 {
     type Error = RecapError;
@@ -73,17 +98,18 @@ where
     }
 }
 
-#[async_trait]
-impl<NB, R, F> CacaoVerifier<UcanSignature, UcanFacts<F>, NB> for CommonVerifier<R>
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+impl<'r, NB, R, F> CacaoVerifier<UcanSignature, UcanFacts<F>, NB> for CommonVerifier<R>
 where
-    R: Send + Sync + DIDResolver,
+    R: 'r + Send + Sync + CacaoVerifier<UcanSignature, UcanFacts<F>, NB>,
     F: Send + Sync + for<'a> Deserialize<'a> + Serialize + Clone,
     NB: Send + Sync + for<'a> Deserialize<'a> + Serialize + Clone,
 {
-    type Error = UcanError;
+    type Error = R::Error;
 
     async fn verify(&self, cacao: &UcanCacao<F, NB>) -> Result<(), Self::Error> {
-        self.0.verify(cacao).await
+        (&self.0).verify(cacao).await
     }
 }
 
@@ -201,5 +227,14 @@ impl<F, NB> TryFrom<CommonCacao<F, NB>> for UcanCacao<F, NB> {
                 signature: VarSig::new(sig),
             }),
         }
+    }
+}
+
+impl<F, NB> TryFrom<Ucan<Common, F, NB>> for CommonCacao<F, NB> {
+    type Error = Error;
+    fn try_from(ucan: Ucan<Common, F, NB>) -> Result<Self, Self::Error> {
+        Ok(UcanCacao::try_from(ucan)
+            .map(Self::from)
+            .map_err(Error::Ucan)?)
     }
 }
